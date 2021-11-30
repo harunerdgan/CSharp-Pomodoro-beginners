f198ff3ce10f4a54ead42256199b2c1b6fd77c81
harunerdgan <83118289+harunerdgan@users.noreply.github.com>
2021-11-30 18:44:24 +0300

    Add files via upload

---
+++ Pomodoro/ponodoro/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿
+namespace ponodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnUzun = new System.Windows.Forms.Button();
+            this.btnKisa = new System.Windows.Forms.Button();
+            this.btnYarım = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDakika.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDakika.Location = new System.Drawing.Point(46, 27);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(107, 76);
+            this.lblDakika.TabIndex = 0;
+            this.lblDakika.Text = "00";
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnBasla.Location = new System.Drawing.Point(8, 120);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(145, 58);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSaniye.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblSaniye.Location = new System.Drawing.Point(205, 27);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(107, 76);
+            this.lblSaniye.TabIndex = 2;
+            this.lblSaniye.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(153, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 76);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ":";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.btnDurdur);
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblDakika);
+            this.panel1.Controls.Add(this.lblSaniye);
+            this.panel1.Location = new System.Drawing.Point(224, 178);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 195);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnDurdur.Location = new System.Drawing.Point(181, 120);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(145, 58);
+            this.btnDurdur.TabIndex = 4;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnUzun
+            // 
+            this.btnUzun.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUzun.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnUzun.Location = new System.Drawing.Point(152, 87);
+            this.btnUzun.Name = "btnUzun";
+            this.btnUzun.Size = new System.Drawing.Size(145, 58);
+            this.btnUzun.TabIndex = 4;
+            this.btnUzun.Text = "25 DK";
+            this.btnUzun.UseVisualStyleBackColor = true;
+            this.btnUzun.Click += new System.EventHandler(this.btnUzun_Click);
+            // 
+            // btnKisa
+            // 
+            this.btnKisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKisa.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnKisa.Location = new System.Drawing.Point(318, 87);
+            this.btnKisa.Name = "btnKisa";
+            this.btnKisa.Size = new System.Drawing.Size(145, 58);
+            this.btnKisa.TabIndex = 5;
+            this.btnKisa.Text = "5 DK";
+            this.btnKisa.UseVisualStyleBackColor = true;
+            this.btnKisa.Click += new System.EventHandler(this.btnKisa_Click);
+            // 
+            // btnYarım
+            // 
+            this.btnYarım.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYarım.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnYarım.Location = new System.Drawing.Point(486, 87);
+            this.btnYarım.Name = "btnYarım";
+            this.btnYarım.Size = new System.Drawing.Size(145, 58);
+            this.btnYarım.TabIndex = 6;
+            this.btnYarım.Text = "20 DK";
+            this.btnYarım.UseVisualStyleBackColor = true;
+            this.btnYarım.Click += new System.EventHandler(this.btnYarım_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Location = new System.Drawing.Point(504, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Long Break";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(339, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Short Break";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(165, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Pomodoro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnYarım);
+            this.Controls.Add(this.btnKisa);
+            this.Controls.Add(this.btnUzun);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnUzun;
+        private System.Windows.Forms.Button btnKisa;
+        private System.Windows.Forms.Button btnYarım;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Pomodoro/ponodoro/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ponodoro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Dakika = 0;
+        int saniye = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDakika.Text = Dakika.ToString();
+            lblSaniye.Text = saniye.ToString();
+            if (saniye==0)
+            {
+                Dakika--; 
+            }
+            if (saniye==0)
+            {
+                saniye = 59;
+            }
+            saniye--;
+            if (Dakika==0)
+            {
+                timer1.Stop();
+                lblDakika.Text = "00";
+                lblSaniye.Text = "00";
+                btnKisa.Enabled = true;
+                btnYarım.Enabled = true;
+                btnUzun.Enabled = true;
+                System.Media.SoundPlayer ses = new System.Media.SoundPlayer();
+                ses.SoundLocation = @"C:\Users\harun\source\repos\ponodoro\ponodoro\ses.wav";
+                ses.Play();
+                MessageBox.Show("Alarmm mola zamanı");
+                ses.Stop();
+
+
+            }
+            if (Dakika<10)
+            {
+                lblDakika.Text = "0" + Dakika.ToString();
+            }
+            if (saniye<10)
+            {
+                lblSaniye.Text = "0" + saniye.ToString(); 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            btnKisa.Enabled = false;
+            btnUzun.Enabled = false;
+            btnYarım.Enabled = false;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            btnBasla.Enabled = false;
+            //SoundPlayer ses = new SoundPlayer();
+            //string dizin = Application.StartupPath + "\\ses";
+            //ses.SoundLocation = dizin;
+            //ses.Play();
+        }
+
+        private void btnUzun_Click(object sender, EventArgs e)
+        {
+            renkler3();
+            Dakika = 24;
+            saniye = 59;
+           
+            btnBasla.Enabled = true;
+
+        }
+
+        private void renkler3()
+        {
+            this.BackColor = Color.IndianRed;
+            btnKisa.ForeColor = Color.IndianRed;
+            btnUzun.ForeColor = Color.IndianRed;
+            btnYarım.ForeColor = Color.IndianRed;
+            btnBasla.ForeColor = Color.IndianRed;
+            btnDurdur.ForeColor = Color.IndianRed;
+            panel1.BackColor = Color.LightCoral;
+        }
+
+        private void btnKisa_Click(object sender, EventArgs e)
+        {
+            Renkler1();
+            Dakika = 4;
+            saniye = 59;
+            
+            btnBasla.Enabled = true;
+
+
+        }
+
+        private void Renkler1()
+        {
+            this.BackColor = Color.LightSeaGreen;
+            btnKisa.ForeColor = Color.MediumTurquoise;
+            btnUzun.ForeColor = Color.MediumTurquoise;
+            btnYarım.ForeColor = Color.MediumTurquoise;
+            btnBasla.ForeColor = Color.MediumTurquoise;
+            btnDurdur.ForeColor = Color.MediumTurquoise;
+            panel1.BackColor = Color.MediumTurquoise;
+        }
+
+        private void btnYarım_Click(object sender, EventArgs e)
+        {
+            renk2();
+            
+            Dakika = 19;
+            saniye = 59;
+
+            
+            btnBasla.Enabled = true;
+
+
+
+        }
+
+        private void renk2()
+        {
+            this.BackColor = Color.SteelBlue;
+            btnKisa.ForeColor = Color.SteelBlue;
+            btnUzun.ForeColor = Color.SteelBlue;
+            btnYarım.ForeColor =Color.SteelBlue;
+            btnBasla.ForeColor =Color.SteelBlue;
+            btnDurdur.ForeColor =Color.SteelBlue;
+            panel1.BackColor = Color.CornflowerBlue;
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            btnKisa.Enabled = true;
+            btnYarım.Enabled = true;
+            btnUzun.Enabled = true;
+            
+        }
+    }
+}
